9bb0785c100f87f17985d6e2453def734dc6acb8
Xavier Shay <contact@xaviershay.com>
2019-08-24 22:09:08 -0700

    It works

--- src/Bypass.cpp
+++ src/Bypass.cpp
@@ -3,106 +3,81 @@
 
 struct Bypass : Module {
 	enum ParamIds {
-		RECT56094_49_6_PARAM,
-		RECT56094_49_PARAM,
-		RECT56094_50_PARAM,
-		RECT56094_17_PARAM,
-		RECT56094_9_PARAM,
-		RECT56094_1_PARAM,
-		RECT56094_5_PARAM,
-		RECT56094_6_PARAM,
-		RECT56094_3_PARAM,
-		RECT56094_4_PARAM,
+    ENUMS(SEND_PARAM,10),
 		NUM_PARAMS
 	};
 	enum InputIds {
-		RECT56657_2_INPUT,
-		RECT56657_INPUT,
-		RECT56657_2_41_INPUT,
-		RECT56657_3_INPUT,
-		RECT56657_2_2_INPUT,
-		RECT56657_9_INPUT,
-		RECT56657_2_42_INPUT,
-		RECT56657_98_INPUT,
-		RECT56657_2_98_INPUT,
-		RECT56657_0_INPUT,
-		RECT56657_2_0_INPUT,
-		RECT56657_1_INPUT,
-		RECT56657_2_1_INPUT,
-		RECT56657_6_INPUT,
-		RECT56657_2_4_INPUT,
-		RECT56657_70_INPUT,
-		RECT56657_2_9_INPUT,
-		RECT56657_62_INPUT,
-		RECT56657_2_5_INPUT,
-		RECT56657_7_INPUT,
+    ENUMS(IN_INPUT,10),
+    ENUMS(RETURN_INPUT,10),
 		NUM_INPUTS
 	};
 	enum OutputIds {
-		RECT56677_OUTPUT,
-		RECT56677_3_OUTPUT,
-		RECT56677_60_OUTPUT,
-		RECT56677_3_6_OUTPUT,
-		RECT56677_0_OUTPUT,
-		RECT56677_3_60_OUTPUT,
-		RECT56677_34_OUTPUT,
-		RECT56677_3_62_OUTPUT,
-		RECT56677_6_OUTPUT,
-		RECT56677_3_9_OUTPUT,
-		RECT56677_28_OUTPUT,
-		RECT56677_3_2_OUTPUT,
-		RECT56677_4_OUTPUT,
-		RECT56677_3_5_OUTPUT,
-		RECT56677_8_OUTPUT,
-		RECT56677_3_1_OUTPUT,
-		RECT56677_25_OUTPUT,
-		RECT56677_3_73_OUTPUT,
-		RECT56677_2_OUTPUT,
-		RECT56677_3_7_OUTPUT,
+    ENUMS(OUT_OUTPUT,10),
+    ENUMS(SEND_OUTPUT,10),
 		NUM_OUTPUTS
 	};
 	enum LightIds {
-		RECT56697_6_7_LIGHT,
-		RECT56697_6_LIGHT,
-		RECT56697_1_LIGHT,
-		RECT56697_57_LIGHT,
-		RECT56697_28_LIGHT,
-		RECT56697_5_LIGHT,
-		RECT56697_3_LIGHT,
-		RECT56697_4_LIGHT,
-		RECT56697_2_LIGHT,
-		RECT56697_0_LIGHT,
+    ENUMS(SEND_LIGHT, 10),
 		NUM_LIGHTS
 	};
 
 	bool state[10];
-	dsp::BooleanTrigger muteTrigger[10];
+	dsp::BooleanTrigger sendTrigger[10];
 
 	Bypass() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		configParam(RECT56094_49_6_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(RECT56094_49_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(RECT56094_50_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(RECT56094_17_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(RECT56094_9_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(RECT56094_1_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(RECT56094_5_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(RECT56094_6_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(RECT56094_3_PARAM, 0.f, 1.f, 0.f, "");
-		configParam(RECT56094_4_PARAM, 0.f, 1.f, 0.f, "");
-    onRandomize();
+    for (int i = 0; i < 10; i++) {
+      configParam(SEND_PARAM + i, 0.f, 1.f, 0.f, "");
+    }
+    onReset();
 	}
+
+  void route(int input, int output) {
+		float out[16] = {};
+		int channels = 1;
+
+    if (inputs[input].isConnected()) {
+      channels = inputs[input].getChannels();
+      inputs[input].readVoltages(out);
+
+      if (outputs[output].isConnected()) {
+        outputs[output].setChannels(channels);
+        outputs[output].writeVoltages(out);
+      }
+    } else {
+      if (outputs[output].isConnected()) {
+        outputs[output].clearVoltages();
+      }
+    }
+  }
 
 	void process(const ProcessArgs &args) override {
     for (int i = 0; i < 10; i++) {
-      lights[RECT56697_6_7_LIGHT+i].setBrightness(state[i] ? 0.9 : 0.0f);
+      if (sendTrigger[i].process(params[SEND_PARAM + i].getValue() > 0.f))
+        state[i] ^= true;
+
+      if (state[i]) {
+        route(IN_INPUT+i, SEND_OUTPUT+i);
+        route(RETURN_INPUT+i, OUT_OUTPUT+i);
+      } else {
+        route(IN_INPUT+i, OUT_OUTPUT+i);
+
+        // Clear send
+        if (outputs[SEND_OUTPUT+i].isConnected()) {
+          outputs[SEND_OUTPUT+i].clearVoltages();
+        }
+      }
+
+      lights[SEND_LIGHT+i].setBrightness(state[i] ? 0.9 : 0.0f);
     }
 	}
+
 	void onReset() override {
 		for (int i = 0; i < 10; i++) {
-			state[i] = true;
+			state[i] = false;
 		}
 	}
+
 	void onRandomize() override {
 		for (int i = 0; i < 10; i++) {
 			state[i] = (random::uniform() < 0.5f);
@@ -127,19 +102,24 @@ struct BypassWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParam<LEDBezel>(mm2px(Vec(26.673, 18.106)), module, Bypass::RECT56094_49_6_PARAM));
-		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 28.337)), module, Bypass::RECT56094_49_PARAM));
-		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 38.51)), module, Bypass::RECT56094_50_PARAM));
-		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 48.683)), module, Bypass::RECT56094_17_PARAM));
-		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 58.855)), module, Bypass::RECT56094_9_PARAM));
-		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 69.028)), module, Bypass::RECT56094_1_PARAM));
-		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 79.201)), module, Bypass::RECT56094_5_PARAM));
-		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 89.373)), module, Bypass::RECT56094_6_PARAM));
-		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 99.546)), module, Bypass::RECT56094_3_PARAM));
-		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 109.719)), module, Bypass::RECT56094_4_PARAM));
+		addParam(createParam<LEDBezel>(mm2px(Vec(26.673, 18.106)), module, Bypass::SEND_PARAM+0));
+		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 28.337)), module, Bypass::SEND_PARAM+1));
+		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 38.51)), module, Bypass::SEND_PARAM+2));
+		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 48.683)), module, Bypass::SEND_PARAM+3));
+		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 58.855)), module, Bypass::SEND_PARAM+4));
+		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 69.028)), module, Bypass::SEND_PARAM+5));
+		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 79.201)), module, Bypass::SEND_PARAM+6));
+		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 89.373)), module, Bypass::SEND_PARAM+7));
+		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 99.546)), module, Bypass::SEND_PARAM+8));
+		addParam(createParam<LEDBezel>(mm2px(Vec(26.73, 109.719)), module, Bypass::SEND_PARAM+9));
 
-		addInput(createInput<PJ301MPort>(mm2px(Vec(15.214, 17.764)), module, Bypass::RECT56657_2_INPUT));
-		addInput(createInput<PJ301MPort>(mm2px(Vec(4.055, 17.81)), module, Bypass::RECT56657_INPUT));
+    for (int i = 0; i < 10; i++) {
+      addInput(createInput<PJ301MPort>(mm2px(Vec(4.055, 17.8 + (10.173 * i))), module, Bypass::IN_INPUT+i));
+      addOutput(createOutput<PJ301MPort>(mm2px(Vec(15.214, 17.8 + (10.173 * i))), module, Bypass::SEND_OUTPUT+i));
+      addInput(createInput<PJ301MPort>(mm2px(Vec(37.533, 17.8 + (10.173 * i))), module, Bypass::RETURN_INPUT+i));
+      addOutput(createOutput<PJ301MPort>(mm2px(Vec(48.692, 17.8 + (10.173 * i))), module, Bypass::OUT_OUTPUT+i));
+    }
+    /*
 		addInput(createInput<PJ301MPort>(mm2px(Vec(15.214, 27.936)), module, Bypass::RECT56657_2_41_INPUT));
 		addInput(createInput<PJ301MPort>(mm2px(Vec(4.055, 27.982)), module, Bypass::RECT56657_3_INPUT));
 		addInput(createInput<PJ301MPort>(mm2px(Vec(15.214, 38.109)), module, Bypass::RECT56657_2_2_INPUT));
@@ -179,19 +159,19 @@ struct BypassWidget : ModuleWidget {
 		addOutput(createOutput<PJ301MPort>(mm2px(Vec(37.533, 99.328)), module, Bypass::RECT56677_3_73_OUTPUT));
 		addOutput(createOutput<PJ301MPort>(mm2px(Vec(48.692, 109.364)), module, Bypass::RECT56677_2_OUTPUT));
 		addOutput(createOutput<PJ301MPort>(mm2px(Vec(37.533, 109.501)), module, Bypass::RECT56677_3_7_OUTPUT));
+    */
 
-		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.423, 18.856)), module, Bypass::RECT56697_6_7_LIGHT));
-		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 29.088)), module, Bypass::RECT56697_6_LIGHT));
-		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 39.261)), module, Bypass::RECT56697_1_LIGHT));
-		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 49.433)), module, Bypass::RECT56697_57_LIGHT));
-		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 59.606)), module, Bypass::RECT56697_28_LIGHT));
-		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 69.779)), module, Bypass::RECT56697_5_LIGHT));
-		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 79.951)), module, Bypass::RECT56697_3_LIGHT));
-		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 90.124)), module, Bypass::RECT56697_4_LIGHT));
-		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 100.297)), module, Bypass::RECT56697_2_LIGHT));
-		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 110.47)), module, Bypass::RECT56697_0_LIGHT));
+		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.423, 18.856)), module, Bypass::SEND_LIGHT));
+		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 29.088)), module, Bypass::SEND_LIGHT+1));
+		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 39.261)), module, Bypass::SEND_LIGHT+2));
+		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 49.433)), module, Bypass::SEND_LIGHT+3));
+		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 59.606)), module, Bypass::SEND_LIGHT+4));
+		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 69.779)), module, Bypass::SEND_LIGHT+5));
+		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 79.951)), module, Bypass::SEND_LIGHT+6));
+		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 90.124)), module, Bypass::SEND_LIGHT+7));
+		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 100.297)), module, Bypass::SEND_LIGHT+8));
+		addChild(createLight<MuteLight<BlueLight>>(mm2px(Vec(27.48, 110.47)), module, Bypass::SEND_LIGHT+9));
 	}
 };
-
 
 Model *modelBypass = createModel<Bypass, BypassWidget>("Bypass");
