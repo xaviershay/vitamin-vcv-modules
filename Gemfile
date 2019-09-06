3d43ceb9a59891c41f9385e63b76c6d263593e10
Xavier Shay <contact@xaviershay.com>
2019-09-06 03:37:03 -0700

    WIP

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'rexml'
+gem 'nokogiri'
+gem 'pry'
---
+++ create_module.rb
@@ -0,0 +1,163 @@
+require 'bundler/setup'
+
+require 'nokogiri'
+require 'pp'
+require 'pry'
+
+Component = Struct.new(:name, :klass, :x, :y) do
+  def self.to_enum_cpp(components)
+    <<-CPP
+  enum #{self.name}Ids {
+#{components.select {|x| self === x }.map {|x| "    %s,\n" % x.name }.join}
+    NUM_#{self.name.upcase}S
+  }
+    CPP
+  end
+end
+
+class Input < Component
+  def to_cpp(module_name)
+    "addInput(createInputCentered<%s>(mm2px(Vec(%f, %f)), module, %s::%s));" % [
+      klass || "PJ301MPort", x, y, module_name, name
+    ]
+  end
+
+  def self.namespace; "INPUT"; end
+end
+
+class Output < Component
+  def to_cpp(module_name)
+    "addOutput(createOutputCentered<%s>(mm2px(Vec(%f, %f)), module, %s::%s));" % [
+      klass || "PJ301MPort", x, y, module_name, name
+    ]
+  end
+
+  def self.namespace; "OUTPUT"; end
+end
+
+class Light < Component
+  def to_cpp(module_name)
+    "addChild(createLightCentered<%s>(mm2px(Vec(%f, %f)), module, %s::%s));" % [
+      klass || "MediumLight<RedLight>", x, y, module_name, name
+    ]
+  end
+
+  def self.namespace; "LIGHT"; end
+end
+
+class Param < Component
+  def to_cpp(module_name)
+    "addParam(createParamCentered<%s>(mm2px(Vec(%f, %f)), module, %s::%s));" % [
+      klass || "RoundBlackKnob", x, y, module_name, name
+    ]
+  end
+
+  def to_config_cpp
+    %|configParam(%s, 0.f, 1.f, 0.f, "")| % name
+  end
+
+  def self.namespace; "PARAM"; end
+end
+
+
+data = File.read("res/Bypass2.svg")
+
+doc = Nokogiri::XML(data)
+
+layer = doc.at_xpath("//svg:g[@inkscape:label='components']") || raise("No components layer found")
+
+module_name = "Bypass2"
+components = []
+
+ALL_COMPONENT_TYPES = [Input, Output, Param, Light]
+ALL_COMPONENT_TYPES.each do |component_type|
+  cs = layer.xpath("//svg:circle[starts-with(@inkscape:label,'#{component_type.namespace}')]")
+  cs.each do |c|
+    name = c['id'].to_s
+    klass = c['inkscape:label'].split("|")[1]
+    x = c['cx'].to_f
+    y = c['cy'].to_f
+    components << component_type.new(name, klass, x, y)
+  end
+
+  gs = layer.xpath("//svg:g[starts-with(@inkscape:label,'#{component_type.namespace}')]")
+  gs.each do |g|
+    transform = g['transform']
+    offset_x, offset_y =
+      if transform
+        matches = transform.match(/translate\((.*),(.*)\)/)
+        if matches
+          [matches[1].to_f, matches[2].to_f]
+        end
+      end || [0, 0]
+    xs = []
+    ys = []
+    g.css('circle').each do |circle|
+      xs << circle['cx'].to_f + offset_x
+      ys << circle['cy'].to_f + offset_y
+    end
+    x = xs.reduce(:+) / xs.length
+    y = ys.reduce(:+) / ys.length
+    name = g['id'].to_s
+    klass = g['inkscape:label'].split("|")[1]
+
+    if name.length == 0
+      raise "No ID on component"
+    end
+    components << component_type.new(name, klass, x, y)
+  end
+end
+
+src = <<-CPP
+#include "plugin.hpp"
+
+struct #{module_name} : Module {
+  /* START GENERATED - DO NOT EDIT: Enums */
+  /* END GENERATED: Enums */
+
+  #{module_name}() {
+    config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+    /* START GENERATED - PARAMS EDITABLE: Param Config */
+    /* END GENERATED: Param Config */
+  }
+
+  void process(const ProcessArgs &args) override {
+  }
+};
+
+struct Bypass2Widget : ModuleWidget {
+  Bypass2Widget(Bypass2 *module) {
+    setModule(module);
+    setPanel(APP->window->loadSvg(asset::plugin(pluginInstance, "res/Bypass2.svg")));
+
+    /* START GENERATED - DO NOT EDIT: Add Components */
+    /* END GENERATED: Add Components */
+  }
+};
+CPP
+
+def replace_generated(src, label, &block)
+  idx_lines = src.lines.each.with_index
+  idx_start = idx_lines.detect {|x, _| x.include?("START GENERATED") && x.include?(label) }[1]
+  idx_end = idx_lines.detect {|x, _| x.include?("END GENERATED") && x.include?(label) }[1]
+
+  lines = src.lines
+  existing = lines.slice!(idx_start+1..idx_end-1)
+  replace = block.call(existing)
+
+  lines = lines.insert(idx_start + 1, *replace.lines)
+  lines.join
+end
+
+def update_src(src, components)
+  src = replace_generated(src, "Enums") do |existing|
+    ALL_COMPONENT_TYPES.map do |type|
+      type.to_enum_cpp(components)
+    end.join("\n")
+  end
+  src = replace_generated(src, "Add Components") do |existing|
+    components.map {|c| "    %s\n" % c.to_cpp("Bypass2") }.join
+  end
+end
+
+puts update_src(src, components)
