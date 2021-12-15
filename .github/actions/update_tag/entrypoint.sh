f9160d9b71a1a41043af5d505abfc674fafccb50
Xavier Shay <contact@xaviershay.com>
2021-12-15 13:01:58 +1100

    Add missing debug output from update_tag

--- .github/actions/update_tag/entrypoint.sh
+++ .github/actions/update_tag/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # expects TAG and REF environment variables set in workflow
 
-set -eu
+set -eux
 
 GITHUB_API_URL=https://api.github.com
 REPO=${GITHUB_REPOSITORY#"https://github.com/"} # work around url parsing bug in act for local testing
